c0b2e389d3674262331cced474aaf5ab227c3dfc
Daniel Pardo <dpardo25@proton.me>
2026-02-12 12:16:57 -0500

    Created publisher and subscriber daemons

---
+++ src/mqtt_daemons/mqtt_publisher.c
@@ -0,0 +1,124 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include "mosquitto.h"
+#include "mqtt_defs.h"
+#include "../aesd_bme280/aesd_bme280_ioctl.h"
+
+#define SENSOR_DRIVER_PATH  "/dev/aesd_bme280"
+
+static struct mosquitto* mosq = NULL;
+static int sensor_fd = -1;
+
+static void cleanup_and_exit(int exit_code)
+{
+    if (mosq != NULL)
+    {
+        mosquitto_destroy(mosq);
+    }
+    mosquitto_lib_cleanup();
+    if (sensor_fd >= 0)
+    {
+        close(sensor_fd);
+    }
+    closelog();
+    exit(exit_code);
+}
+
+static void signal_handler(int signal_number)
+{
+    if ((signal_number == SIGINT) || (signal_number == SIGTERM))
+    {
+        syslog(LOG_INFO, "Caught signal %d, exiting", signal_number);
+        cleanup_and_exit(EXIT_SUCCESS);
+    }
+}
+
+static void init_signal_handler()
+{
+    struct sigaction signal = { .sa_handler = signal_handler };
+    if (sigaction(SIGINT, &signal, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Unable to register SIGINT");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGTERM, &signal, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Unable to register SIGTERM");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int result;    
+    struct aesd_bme280_data sensor_data;
+    char mqtt_message[128];
+
+    openlog(NULL, 0, LOG_USER);
+    init_signal_handler();
+
+    mosquitto_lib_init();
+    mosq = mosquitto_new(NULL, true, NULL);
+    if (!mosq)
+    {
+        syslog(LOG_ERR, "Failed to create Mosquitto instance");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+
+    // Connect to MQTT broker
+    if ((result = mosquitto_connect(mosq, MQTT_BROKER_ADDR, MQTT_BROKER_PORT, 60)) != MOSQ_ERR_SUCCESS)
+    {
+        syslog(LOG_ERR, "Failed to connect to MQTT broker: %s", mosquitto_strerror(result));
+        cleanup_and_exit(result);
+    }
+
+    // Open sensor
+    sensor_fd = open(SENSOR_DRIVER_PATH, O_RDONLY);
+    if (sensor_fd < 0)
+    {
+        syslog(LOG_ERR, "Failed to open device %s: %s\n", SENSOR_DRIVER_PATH, strerror(errno));
+        result = errno;
+        cleanup_and_exit(result);
+    }
+
+    // Daemonize the process
+    if (daemon(0, 0) < 0)
+    {
+        syslog(LOG_ERR, "Failed to create daemon: %s", strerror(errno));
+    }
+
+    while (1)
+    {
+        // Read sensor values
+        if ((result = ioctl(sensor_fd, AESD_BME280_GET_ALL, &sensor_data)) < 0)
+        {
+            syslog(LOG_ERR, "Failed to get sensor data: %s\n", strerror(result));
+        }
+        else
+        {
+            // Format and publish message
+            sprintf(mqtt_message, "Temperature: %d.%02d°C, Pressure: %u.%02uPa, Humidity: %u.%01u%%",
+                sensor_data.temperature / 100,  abs(sensor_data.temperature) % 100,
+                sensor_data.pressure / 256,     (sensor_data.pressure % 256) * 100 / 256,
+                sensor_data.humidity / 1024,    (sensor_data.humidity % 1024) * 10 / 1024
+            );
+
+            if ((result = mosquitto_publish(mosq, NULL, MQTT_TOPIC, sizeof(mqtt_message), mqtt_message, 0, false)) != MOSQ_ERR_SUCCESS)
+            {
+                syslog(LOG_ERR, "Failed to publish message to topic %s", mosquitto_strerror(result));
+            }
+        }
+
+        // Sleep for 10 seconds
+        sleep(10);
+    }
+
+    return 0;
+}
---
+++ src/mqtt_daemons/mqtt_subscriber.c
@@ -0,0 +1,133 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <gpiod.h>
+#include "mosquitto.h"
+#include "mqtt_defs.h"
+
+#define LED_GPIO_CHIP_PATH  "/dev/gpiochip0"
+#define LED_GPIO_PIN        26
+
+static struct mosquitto* mqtt_client = NULL;
+static struct gpiod_chip* led_gpio_chip;
+static struct gpiod_line* led_gpio_line;
+
+static void cleanup_and_exit(int exit_code)
+{
+    if (mqtt_client != NULL)
+    {
+        mosquitto_destroy(mqtt_client);
+    }
+    mosquitto_lib_cleanup();
+    if (led_gpio_line != NULL)
+    {
+        gpiod_line_release(led_gpio_line);
+    }
+    if (led_gpio_chip != NULL)
+    {
+        gpiod_chip_close(led_gpio_chip);
+    }
+    closelog();
+    exit(exit_code);
+}
+
+static void signal_handler(int signal_number)
+{
+    if ((signal_number == SIGINT) || (signal_number == SIGTERM))
+    {
+        syslog(LOG_INFO, "Caught signal %d, exiting", signal_number);
+        cleanup_and_exit(EXIT_SUCCESS);
+    }
+}
+
+static void init_signal_handler()
+{
+    struct sigaction signal = { .sa_handler = signal_handler };
+    if (sigaction(SIGINT, &signal, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Unable to register SIGINT");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGTERM, &signal, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Unable to register SIGTERM");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+}
+
+static void mqtt_message_callback(struct mosquitto* mosq, void* obj, const struct mosquitto_message* message)
+{
+    if (message->payloadlen > 0)
+    {
+        syslog(LOG_INFO, "Received MQTT message on topic %s: %s", message->topic, (char*)message->payload);
+
+        // Toggle LED state
+        int current_value = gpiod_line_get_value(led_gpio_line);
+        if (current_value < 0)
+        {
+            syslog(LOG_ERR, "Failed to read GPIO line value.");
+            return;
+        }
+        gpiod_line_set_value(led_gpio_line, !current_value);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int result;    
+    openlog(NULL, 0, LOG_USER);
+    init_signal_handler();
+
+    // Obtain GPIO pin
+    if ((led_gpio_chip = gpiod_chip_open(LED_GPIO_CHIP_PATH)) == NULL)
+    {
+        syslog(LOG_ERR, "Failed to open GPIO chip 0.");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+    if ((led_gpio_line = gpiod_chip_get_line(led_gpio_chip, LED_GPIO_PIN)) == NULL)
+    {
+        syslog(LOG_ERR, "Failed to get GPIO %d.", LED_GPIO_PIN);
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+    if (gpiod_line_request_output(led_gpio_line, "aesdled", 1) != 0)
+    {
+        syslog(LOG_ERR, "GPIO line output request failed.");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+
+    mosquitto_lib_init();
+    mqtt_client = mosquitto_new(NULL, true, NULL);
+    if (!mqtt_client)
+    {
+        syslog(LOG_ERR, "Failed to create Mosquitto instance");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+
+    mosquitto_message_callback_set(mqtt_client, mqtt_message_callback);
+
+    // Connect to MQTT broker
+    if ((result = mosquitto_connect(mqtt_client, MQTT_BROKER_ADDR, MQTT_BROKER_PORT, 60)) != MOSQ_ERR_SUCCESS)
+    {
+        syslog(LOG_ERR, "Failed to connect to MQTT broker: %s", mosquitto_strerror(result));
+        cleanup_and_exit(result);
+    }
+    
+    mosquitto_subscribe(mqtt_client, NULL, MQTT_TOPIC, 0);
+    
+    // Daemonize the process
+    if (daemon(0, 0) < 0)
+    {
+        syslog(LOG_ERR, "Failed to create daemon: %s", strerror(errno));
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+
+    mosquitto_loop_start(mqtt_client);
+    
+    while (1);
+    return 0;
+}
